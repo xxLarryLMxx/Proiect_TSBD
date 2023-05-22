77554be63a43e45b31daee2aa16388e7514e7532
Andrei <andreipanfil@yahoo.com>
2023-05-22 21:49:10 +0300

    Initial commit

---
+++ src/main/java/dwbi/proiect_dwbi/ProiectDwbiApplication.java
@@ -0,0 +1,18 @@
+package dwbi.proiect_dwbi;
+
+import dwbi.proiect_dwbi.repository.InvoiceRepository;
+import dwbi.proiect_dwbi.service.InvoiceService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectDwbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectDwbiApplication.class, args);
+
+
+
+	}
+
+}
